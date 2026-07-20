36ab973bd1f9bc22859e32dece07643ca74e7e2f
jawad66108 <jawad66108@gmail.com>
2026-07-20 16:22:05 +0500

    Up till this point, my Phase 1 is completed. Now I'll be moving to more difficult concepts.

--- Day06.js
+++ Day06.js
@@ -4,10 +4,12 @@
 // ArrowMul(3, 5);
 // console.log(ArrowMul);
 
+// const { useTransition } = require("react");
+
 //Function the returns vowels
 
-let st = "";
-let vowels = "AEIOUaeiou";
+// let st = "";
+// let vowels = "AEIOUaeiou";
 
 // function Rvowels(s) {
 //   for (let i = 0; i < s.length; i++) {
@@ -90,15 +92,146 @@ let vowels = "AEIOUaeiou";
 
 // console.log(s1, s2, s3);
 
-function Student(name, marks) {
-  this.name = name;
-  this.marks = marks;
-  this.ispassed = function () {
-    this.marks >= 50
-      ? console.log(`${this.name} is Passed`)
-      : console.log(`${this.name} is failed`);
-  };
+// function Student(name, marks) {
+//   this.name = name;
+//   this.marks = marks;
+//   this.ispassed = function () {
+//     this.marks >= 50
+//       ? console.log(`${this.name} is Passed`)
+//       : console.log(`${this.name} is failed`);
+//   };
+// }
+
+// let s1 = new Student("Jawad", 81);
+// s1.ispassed();
+
+// class emp {
+//   constructor(name, salary) {
+//     this.name = name;
+//     this.salary = salary;
+//   }
+//   giveBonus() {
+//     return (this.salary += this.salary * 0.1);
+//   }
+// }
+
+// let emp1 = new emp("Mohsin", 1000);
+// console.log(emp1.salary);
+// emp1.giveBonus();
+// console.log(emp1.salary);
+
+// class BankAccount {
+//   constructor(accHolder, balance) {
+//     this.accHolder = accHolder;
+//     this.balance = balance;
+//   }
+
+//   deposit(amount) {
+//     this.balance += amount;
+//   }
+
+//   withdraw(amount) {
+//     this.balance -= amount;
+//   }
+// }
+
+// let P1 = new BankAccount("Hamza Khan", 4000);
+// console.log(P1);
+// P1.deposit(30);
+// console.log(P1);
+// P1.withdraw(1000);
+// console.log(P1);
+
+// class Rectangle {
+//   constructor(length, width) {
+//     this.length = length;
+//     this.width = width;
+//   }
+
+//   area() {
+//     console.log(this.length * this.width);
+//   }
+
+//   perimeter() {
+//     console.log("Have to look in for formula");
+//   }
+// }
+
+// let r = new Rectangle(5, 4);
+
+// r.area();
+
+// r.perimeter();
+
+// class Animal {
+//   constructor(name) {
+//     this.name = name;
+//   }
+
+//   eat() {
+//     console.log("Every animal must eat");
+//   }
+// }
+
+// class Dog extends Animal {
+//   constructor(name, bread) {
+//     super(name);
+//     this.bread = bread;
+//   }
+//   bark() {
+//     console.log("Dogs brak to much");
+//   }
+// }
+
+// let c = new Dog();
+// console.log(c.bark);
+// // console.log(c.eat);
+
+// class emp {
+//   constructor(name, salary) {
+//     this.name = name;
+//     this.salary = salary;
+//   }
+// }
+
+// class manager extends emp {
+//   constructor(name, salary, dep) {
+//     super(name, salary);
+//     this.dep = dep;
+//   }
+
+//   showdetails() {
+//     console.log(
+//       `Name: ${this.name} Salary: ${this.salary} Department: ${this.dep}`,
+//     );
+//   }
+// }
+
+// let emp1 = new manager("Mahnoor Saleem", 1000, "CS");
+// emp1.showdetails();
+
+class user {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+  }
+
+  login() {
+    return "User successfully login";
+  }
 }
 
-let s1 = new Student("Jawad", 81);
-s1.ispassed();
+class admin extends user {
+  constructor(username, email, role) {
+    super(username, email);
+    this.role = role;
+  }
+  delUser() {
+    return "The that was login is now deleted";
+  }
+}
+
+let A = new admin("Mohsin Khan", "Ronaldo@C.R", "Captain");
+
+A.login();
+A.delUser();
