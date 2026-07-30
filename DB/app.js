1769b6bce7f1deb94a761e2c73e4ff7a7c755d4d
jawad66108 <jawad66108@gmail.com>
2026-07-30 16:35:30 +0500

    fully learned to use get api with backend and practiced alot of question as well next i will be moving to post apis with backend

--- DB/app.js
+++ DB/app.js
@@ -3,76 +3,105 @@ import pool from "./connection.js";
 
 let app = express();
 
-app.get("/api/std", async (req, res) => {
-  console.log("all students ");
+// app.get("/api/std", async (req, res) => {
+//   console.log("all students ");
+//   try {
+//     let data = await pool.query("select*from students");
+//     res.json(data.rows);
+//   } catch (err) {
+//     res.status(500).json({ err: err.message });
+//   }
+// });
+
+// app.get("/api/std/:id", async (req, res) => {
+//   try {
+//     let nid = Number(req.params.id);
+//     let data = await pool.query("select * from students where id = $1 ", [nid]);
+//     res.json(data.rows[0]);
+//   } catch (err) {
+//     res.status(500).json({ err: "Not Found!!!" });
+//   }
+// });
+
+// app.get("/api/std", async (req, res) => {
+//   try {
+//     console.log("filtered students");
+//     let agge = Number(req.query.age);
+
+//     let data = await pool.query("select name from students where age>=$1", [
+//       agge,
+//     ]);
+
+//     if (data.rows.length === 0)
+//       return res.json({ msg: `No student present under this ${agge}` });
+
+//     res.json(data.rows);
+//   } catch (err) {
+//     res.status(500).json({ err: "internal Server Error" });
+//   }
+// });
+
+// app.get("/api/std/sort/:orderby", async (req, res) => {
+//   let oorderby = req.params.orderby;
+
+//   let data = await pool.query(`select * from students order by ${oorderby}`);
+
+//   res.json(data.rows);
+// });
+
+// app.get("/api/std/search", async (req, res) => {
+//   try {
+//     let age = req.query.age;
+//     let name = req.query.name;
+
+// let col = ["age", "name"];
+
+// if (!{ age, name }.include(col))
+//   return res
+//     .status(404)
+//     .json({ err: "Not found the col you are looking for" });
+
+//     if (agge && naame) {
+//       let data = await pool.query(`select name,age from students`);
+//       res.json(data.rows);
+//     } else if (!agge && naame) {
+//       let data = await pool.query(`select name from students`);
+//       res.json(data.rows);
+//     } else {
+//       let data = await pool.query(`select age from students`);
+//       res.json(data.rows);
+//     }
+//   } catch (err) {
+//     res.status(500).json({ err: `Unknow error in Backend` });
+//   }
+// });
+
+app.get("/api/std/search/", async (req, res) => {
+  let Sname = req.query.name;
+  let Sage = req.query.age;
+
   try {
-    let data = await pool.query("select*from students");
-    res.json(data.rows);
-  } catch (err) {
-    res.status(500).json({ err: err.message });
-  }
-});
-
-app.get("/api/std/:id", async (req, res) => {
-  try {
-    let nid = Number(req.params.id);
-    let data = await pool.query("select * from students where id = $1 ", [nid]);
-    res.json(data.rows[0]);
-  } catch (err) {
-    res.status(500).json({ err: "Not Found!!!" });
-  }
-});
-
-app.get("/api/std", async (req, res) => {
-  try {
-    console.log("filtered students");
-    let agge = Number(req.query.age);
-
-    let data = await pool.query("select name from students where age>=$1", [
-      agge,
-    ]);
-
-    if (data.rows.length === 0)
-      return res.json({ msg: `No student present under this ${agge}` });
-
-    res.json(data.rows);
-  } catch (err) {
-    res.status(500).json({ err: "internal Server Error" });
-  }
-});
-
-app.get("/api/std/sort/:orderby", async (req, res) => {
-  let oorderby = req.params.orderby;
-
-  let data = await pool.query(`select * from students order by ${oorderby}`);
-
-  res.json(data.rows);
-});
-
-app.get("/api/std/search", async (req, res) => {
-  try {
-    let age = req.query.age;
-    let name = req.query.name;
-
-    // let col = ["age", "name"];
-
-    // if (!{ age, name }.include(col))
-    //   return res
-    //     .status(404)
-    //     .json({ err: "Not found the col you are looking for" });
-
-    if (agge && naame) {
-      let data = await pool.query(`select name,age from students`);
+    if (Sname && Sage) {
+      let data = await pool.query(
+        `select * from students where name = $1 AND age = $2`,
+        [Sname, Sage],
+      );
+      return res.json(data.rows);
+    } else if (Sname && !Sage) {
+      let data = await pool.query(`select  * from students where name = $1`, [
+        Sname,
+      ]);
       res.json(data.rows);
-    } else if (!agge && naame) {
-      let data = await pool.query(`select name from students`);
+    } else if (!Sname && Sage) {
+      let data = await pool.query(`select * from students where age=$1`, [
+        Sage,
+      ]);
       res.json(data.rows);
     } else {
-      let data = await pool.query(`select age from students`);
-      res.json(data.rows);
+      res.json({ err: "You can only search from name and age" });
     }
   } catch (err) {
-    res.status(500).json({ err: `Unknow error in Backend` });
+    res.status(500).json({ err: err.message });
   }
 });
 
