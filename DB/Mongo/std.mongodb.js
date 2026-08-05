62a84ad25f3d17b9876148c8efb8dcdee40c0626
jawad66108 <jawad66108@gmail.com>
2026-08-05 11:18:54 +0500

    Inserted data into DB bcz going to backend i wanna strength this

--- DB/Mongo/std.mongodb.js
+++ DB/Mongo/std.mongodb.js
@@ -1,3 +1,15 @@
 use("ecommerce");
 
 // db.students.find({ age: { $gt: 20 } }, { _id: 0 });
+
+// db.students.find().sort({ age: 1 }).limit(3);
+// db.students.find({}, { name: 1, age: 1, _id: 0 });
+
+// db.students.find({ city: { $in: ["Lahore", "Islamabad"] } }, { _id: 0 });
+
+// db.students.find({ grade: "A" });
+
+db.students.updateOne(
+  { name: "Ali" },
+  { $set: { subjects: ["Math", "Physics", "AI"] } },
+);
