b088c3a853f98d8167296c1393e169981171a84b
jawad66108 <jawad66108@gmail.com>
2026-08-10 12:18:17 +0500

    Restore React and MongoDB files

---
+++ SomeReact/C1.jsx
@@ -0,0 +1,11 @@
+function std(props) {
+  return (
+    <>
+      <h1>Student Information</h1>
+      <p1>{props.name} </p1>
+      <p2>{props.program} </p2>
+    </>
+  );
+}
+
+export default std;
