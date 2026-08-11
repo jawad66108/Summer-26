cdb9966036e4263a33797ad479f28c2db25d162b
jawad66108 <jawad66108@gmail.com>
2026-08-11 11:10:33 +0500

    Learning the authentication here and pracice it at very bacis level

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ import productsRouter from "./routes/GetOnes.js";
 import createP from "./routes/GetMore.js";
 import update from "./routes/puting.js";
 import del from "./routes/del.js";
+import auth from "./routes/auth.js";
 
 connectDB();
 
 let app = express();
 app.use(express.json());
 
+app.use("/api", auth);
 app.use("/api/products", productsRouter);
 app.use("/api/products/Creat", createP);
 app.use("/api/putting", update);
