cb2459a8097dbb3a0e391e0077557e6abdd2c3cf
Jawad Hussain <jawad66108@gmail.com>
2026-08-13 12:32:15 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 import express from "express";
-import connectDB from "./connections.js";
-import productsRouter from "./routes/GetOnes.js";
-import createP from "./routes/GetMore.js";
-import update from "./routes/puting.js";
-import del from "./routes/del.js";
-import auth from "./routes/auth.js";
+import login from "./routes/login.js";
+import db from "./DB/connection.js";
+import test from "./routes/test.js";
 
-connectDB();
+db();
 
 let app = express();
 app.use(express.json());
 
 app.use("/api", auth);
-app.use("/api/products", productsRouter);
-app.use("/api/products/Creat", createP);
-app.use("/api/putting", update);
-app.use("/api/deleting", del);
+app.use("/api", test);
+app.use("/api", login);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
