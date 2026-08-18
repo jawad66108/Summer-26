6d2d7bb3befc3dc47259595b0d2cb14cb9ee0f23
jawad66108 <jawad66108@gmail.com>
2026-08-18 17:08:43 +0500

    now today i have also completey build the api calls and end point with testing for moudule 1

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import db from "./DB/connection.js";
 import dotenv from "dotenv";
 import auth from "./Auth/login.js";
+import items from "./routes/items.js";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ let app = express();
 app.use(express.json());
 
 app.use("/api", auth);
+app.use("/api", items);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running successfully runing`);
